2bb1d56b62edc33426683b68e11c47c583914819
germanpagano <sergiogermanpagano@gmail.com>
2024-07-03 23:22:06 -0300

    upload

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const alumnos =[
     {id:"3" , nombre: "cristian" }
 ]
 
+
+// params
 app.get('/alumnos/:idAlumno', (req, res)=>{
     let idAlumno = req.params.idAlumno
     let alumno = alumnos.find(e => e.id === idAlumno) 
@@ -37,3 +39,17 @@ app.get('/alumnos/:idAlumno', (req, res)=>{
 
 
 //los filtros y paginados de los datos que mostramos se hace en el backend
+// todos los parametros recibidos de la url son siempre string
+// como limitar la cantidad de alumnos a mostrar
+
+
+//query , este permite concatenar filtrar en varias instancias (&&)
+app.get('/limitados', (req, res) => {
+    let limit = parseInt(req.query.limit)
+    let limitedAlumnos = [...alumnos]
+    if ( !isNaN(limit) && limit > 0){
+        limitedAlumnos = limitedAlumnos.slice(0,limit)// limitamos la cantidad
+    }
+    res.json(limitedAlumnos)
+})
+//http://localhost:8080/limitados?limit=2
